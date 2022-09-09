3f0a5d17d0bb60964ac2d4cc7b3d68a2c74e28b5
Jay <kubby159@naver.com>
2022-09-09 23:33:01 +0900

    Fixed Error through Selenium

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
 from requests import get
 from extractors.wwr import extract_wwr_jobs
 
-
-jobs = extract_wwr_jobs('python')
-print(jobs)
-
-
-
-
-
-
-
+base_url = 'https://kr.indeed.com/jobs?q='
+search_term = 'python'
+chrome_options = webdriver.ChromeOptions()
+browser = webdriver.Chrome(service = Service(ChromeDriverManager().install()), options = chrome_options)
+browser.get(f"{base_url}{search_term}")
+print(browser.page_source)
+time.sleep(2)
+browser.close()
