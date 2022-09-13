3a3b7dd9943c868b292013516b446a47e908767f
Jay <kubby159@naver.com>
2022-09-13 23:31:21 +0900

    pagination scrapping

--- main.py
+++ main.py
@@ -6,31 +6,50 @@ import time
 from requests import get
 from extractors.wwr import extract_wwr_jobs
 
-base_url = 'https://kr.indeed.com/jobs?q='
-search_term = 'python'
-chrome_options = webdriver.ChromeOptions()
-browser = webdriver.Chrome(service = Service(ChromeDriverManager().install()), options = chrome_options)
-browser.get(f'{base_url}{search_term}')
-soup = BeautifulSoup(browser.page_source, 'html.parser')
-job_list = soup.find('ul',class_='jobsearch-ResultsList')
-jobs = job_list.find_all('li',recursive=False)
-results = []
-for job in jobs:
-    zone = job.find('div', class_='mosaic-zone')
-    if zone == None:
-        anchor = job.select_one("h2 a")
-        title = anchor['aria-label']
-        link = anchor['href']
-        company = job.find('span', class_='companyName')
-        location = job.find('div', class_='companyLocation')
-        job_data = {
-            "link":f"https://kr.indeed.com{link}",
-            "company":company.string,
-            "location":location.string,
-            "position":title
-        }
-        results.append(job_data)
+
+def get_page_count(keyword):
+    base_url = 'https://kr.indeed.com/jobs?q='
+    chrome_options = webdriver.ChromeOptions()
+    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    browser.get(f'{base_url}{keyword}')
+    browser.implicitly_wait(10)
+    soup = BeautifulSoup(browser.page_source, 'html.parser')
+    page_lists = soup.select('ul.pagination-list li')
+
+    if page_lists == []:
+        print("There're not Paginaiton")
+    else:
+        for page in page_lists:
+            print(page.text)
 
 
-for result in results:
-    print(result, "\n///////\n")
+get_page_count('nest')
+
+
+def extract_indeed_jobs(keyword):
+    base_url = 'https://kr.indeed.com/jobs?q='
+    chrome_options = webdriver.ChromeOptions()
+    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    browser.get(f'{base_url}{keyword}')
+    soup = BeautifulSoup(browser.page_source, 'html.parser')
+    job_list = soup.find('ul', class_='jobsearch-ResultsList')
+    jobs = job_list.find_all('li', recursive=False)
+    results = []
+    for job in jobs:
+        zone = job.find('div', class_='mosaic-zone')
+        if zone == None:
+            anchor = job.select_one("h2 a")
+            title = anchor['aria-label']
+            link = anchor['href']
+            company = job.find('span', class_='companyName')
+            location = job.find('div', class_='companyLocation')
+            job_data = {
+                "link": f"https://kr.indeed.com{link}",
+                "company": company.string,
+                "location": location.string,
+                "position": title
+            }
+            results.append(job_data)
+
+    for result in results:
+        print(result, "\n///////\n")
